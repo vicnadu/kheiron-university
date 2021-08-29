9bac26e92a08689b952c5c6ac004e2a292d6411c
Victor Nadu <78039394+vicnadu@users.noreply.github.com>
2021-08-29 12:27:23 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    responsiveWidth: 768,
+    navigation: true,
+    anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6']
+})
